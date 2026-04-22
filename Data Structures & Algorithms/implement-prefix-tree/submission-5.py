94408baa3dcd8c5c79a87d64ae6b939989385aa6
NarboMast <narboto.az2005@gmail.com>
2026-04-22 18:23:20 +0200

    Add: implement-prefix-tree - submission-5

---
+++ Data Structures & Algorithms/implement-prefix-tree/submission-5.py
@@ -0,0 +1,37 @@
+class TrieNode:
+    def __init__(self):
+        self.children = {}
+        self.word_end = False
+
+class PrefixTree:
+    def __init__(self):
+        self.root = TrieNode()
+
+    def insert(self, word: str) -> None:
+        curr = self.root
+        for c in word:
+            if c not in curr.children:
+                curr.children[c] = TrieNode()
+                
+            curr = curr.children[c]
+        curr.word_end = True
+
+    def search(self, word: str) -> bool:
+        curr = self.root
+        for c in word:
+            if c in curr.children:
+                curr = curr.children[c]
+            else:
+                return False
+
+        return curr.word_end
+
+    def startsWith(self, prefix: str) -> bool:
+        curr = self.root
+        for c in prefix:
+            if c in curr.children:
+                curr = curr.children[c]
+            else:
+                return False
+        return True
+        
