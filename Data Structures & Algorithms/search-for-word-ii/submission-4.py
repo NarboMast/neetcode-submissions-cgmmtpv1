e52c76d92b7127b800b416ce1d9fdb92c74043cb
NarboMast <narboto.az2005@gmail.com>
2026-04-22 23:02:20 +0200

    Add: search-for-word-ii - submission-4

---
+++ Data Structures & Algorithms/search-for-word-ii/submission-4.py
@@ -0,0 +1,84 @@
+class TrieNode:
+    def __init__(self):
+        self.children = {}
+        self.word_end = False
+
+class TrieTree:
+    def __init__(self):
+        self.root = TrieNode()
+    
+    def insert(self, word):
+        curr = self.root
+
+        for c in word:
+            if c not in curr.children:
+                curr.children[c] = TrieNode()
+            curr = curr.children[c]
+        
+        curr.word_end = True
+
+    def search(self, word):
+        curr = self.root
+
+        for c in word:
+            if c not in curr.children:
+                return False
+            curr = curr.children[c]
+        return curr.word_end
+
+    def starts_with(self, sub_word):
+        curr = self.root
+
+        for c in sub_word:
+            if c not in curr.children:
+                return False
+            curr = curr.children[c]
+        return True
+
+class Solution:
+    def findWords(self, board: List[List[str]], words: List[str]) -> List[str]:
+        root = TrieTree()
+        for word in words:
+            root.insert(word)
+
+        directions = (
+            (0, -1), #left
+            (-1, 0), #up
+            (0, 1), #right
+            (1, 0) #down
+        )
+        ROWS = len(board)
+        COLS = len(board[0])
+
+        res = set()
+
+        def dfs(r, c, word, visited, node):
+            if (
+                r < 0 or r >= ROWS or
+                c < 0 or c >= COLS or
+                (r, c) in visited
+            ): return
+
+            char = board[r][c]
+            if char not in node.children:
+                return
+
+            visited.add((r, c))
+            node = node.children[char]
+            new_word = word + char
+
+            if node.word_end:
+                res.add(new_word)
+
+            for rd, cd in directions:
+                dfs(r + rd, c + cd, new_word, visited, node)
+
+            visited.remove((r,c))
+
+        for r in range(ROWS):
+            for c in range(COLS):
+                visited = set()
+                dfs(r, c, '', visited, root.root)
+
+        return list(res)
+        
